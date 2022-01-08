27bc7f812658e4459e48bd1157ccbdd8657b6e74
mmarienko <maksym.marienko@gmail.com>
2022-01-08 02:44:14 +0200

    Assignment 7: Time to Practice: Data & Functions

--- 19-the-composition-api/src/App.vue
+++ 19-the-composition-api/src/App.vue
@@ -1,16 +1,64 @@
 <template>
   <section class="container">
     <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script>
+import { ref, computed } from 'vue';
+
 export default {
-  data() {
+  setup() {
+    // const uName = ref('Maximilian');
+    const firstName = ref('');
+    const lastName = ref('');
+    const uAge = ref(31);
+    // const user = reactive({
+    //   name: 'Maximilian',
+    //   age: 31,
+    // });
+
+    const uName = computed(function() {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    function setNewAge() {
+      uAge.value = 33;
+    }
+
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+
+    function setLastName(event) {
+      lastName.value = event.target.value;
+    }
+
     return {
-      userName: 'Maximilian',
+      userName: uName,
+      age: uAge,
+      setAge: setNewAge,
+      setFirstName,
+      setLastName,
     };
   },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //     age: 31
+  //   };
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 32;
+  //   }
+  // }
 };
 </script>
 
